96ce806f3cf7e580346304ab91d2bf9e5d4db0ea
weltonperes <158831972+weltonperes@users.noreply.github.com>
2024-03-20 19:47:30 -0300

    curriculo

---
+++ script.js
@@ -0,0 +1,11 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementByIdt ('peso');
+const calcular = document.getElementByIdt ('calcular');
+const resultado = document.getElementByIdt ('resultado');
+
+
+function imc (){
+    alert('teste click')
+}
+calcular.addEventListener('click', imc)
